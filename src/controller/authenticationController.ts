4fa99a95d6cbf570828baba3c6a8f3f34e32ff63
Serhii Temchenko <serhii.t@jolocom.com>
2021-07-12 11:50:48 +0300

    feat(authentication): added authentication endpoint implementation

--- src/controller/authenticationController.ts
+++ src/controller/authenticationController.ts
@@ -1,9 +1,26 @@
 import { controller, httpPost } from 'inversify-express-utils'
+import { SdkAgentFactory } from '../sdk/sdkAgentFactory'
+import { RequestDescriptionFactory } from '../interaction/requestDescriptionFactory'
+import { Request } from 'express'
+import config from '../config/config'
 
 @controller('/authentication')
 export class AuthenticationController {
+  constructor(
+    private readonly agentFactory: SdkAgentFactory,
+    private readonly requestDescriptionFactory: RequestDescriptionFactory,
+  ) {}
+
   @httpPost('/')
-  public async post() {
-    // TODO: Implement
+  public async post(request: Request) {
+    const agent = await this.agentFactory.create()
+    // TODO: Refactor in favor of strategy pattern usage
+    const token = await agent.authRequestToken({
+      description: request.body?.description,
+      callbackURL: config.sdk.callbackUrl,
+    })
+    const requestDescription = await this.requestDescriptionFactory.create(token)
+
+    return requestDescription.toJSON()
   }
 }
