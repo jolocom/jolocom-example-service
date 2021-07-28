1ba6f16eba7099bbbf476a2ce261d625d8520301
Serhii Temchenko <serhii.t@jolocom.com>
2021-07-28 12:07:45 +0300

    feat(assertion): added common assertion method

--- src/assert/assert.ts
+++ src/assert/assert.ts
@@ -1,4 +1,5 @@
 import { AssertionError } from 'assert'
+import { InvalidArgumentException } from '../exception/invalidArgumentException'
 
 export class Assert {
   public static isDefined<T>(value: T, propertyName?: string): asserts value is NonNullable<T> {
@@ -8,6 +9,12 @@ export class Assert {
           ? `Expected 'value' of property '${propertyName}' to be defined, but received '${value}'`
           : `Expected 'value' to be defined, but received '${value}'`
       })
+    }
+  }
+
+  public static true(value: any, message: string | null = null) {
+    if (value !== true) {
+      throw new InvalidArgumentException(message || `Expected a value to be true. Got: ${value}`)
     }
   }
 }
